265658cb68653ac3a5a4c2b193e7c7f886667244
saikat-data <saikatmaji43@gmail.com>
2026-08-14 19:47:29 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,20 @@
+//import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+         return false;
+    }
+char[] a = s.toCharArray();
+char[] b = t.toCharArray();
+ 
+ Arrays.sort(a);
+ Arrays.sort(b);
+ // return Arrays.equals(a,b);
+if(Arrays.equals(a, b)){
+    return true;
+}else{
+    return false;
+}
+//return 0;
+}
+}
